39bdc72c8074cf4445b43be0d91fd47a4e76c2d6
huangsiqin <450555286@qq.com>
2020-12-14 10:05:34 +0800

    新建二叉树

---
+++ 思沁实现树.js
@@ -0,0 +1,52 @@
+class Node {
+    constructor (element, parent) {
+        this.element = element
+        this.parent = parent
+        this.left = null
+        this.right = null
+    }
+}
+
+class Tree {
+    constructor () {
+        this.root = null
+    }
+    compare(e1,e2){
+        console.log(e1,e2)
+        return e1 > e2
+    }
+    add (element) {
+        if (this.root == null) {
+            this.root = new Node(element, null)
+            return
+        }
+        let currentNode = this.root
+        let parent
+        let compare
+        while (currentNode) {
+            compare = this.compare(currentNode.element,element)
+            parent = currentNode
+            if (compare) {
+                currentNode = currentNode.left
+            } else {
+                currentNode = currentNode.right
+            }
+        }
+        let node = new Node(element, parent)
+        if (compare) {
+            parent.left = node
+        } else {
+            parent.right = node
+        }
+    }
+}
+
+let tree = new Tree()
+tree.add(10);
+tree.add(8); // 我的逻辑就认为 不能存放相同的节点
+tree.add(19);
+tree.add(6);
+tree.add(15);
+tree.add(22);
+tree.add(20);
+console.dir(tree,{depth:1000})
