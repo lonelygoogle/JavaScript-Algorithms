11685bb69cab17095763a36c282f3a24888e7dfb
steven huang <steven.huang@highlimitstudio.com>
2023-07-13 21:44:38 +1000

    1st java solution for leetcode

---
+++ java/1.two sum.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+class Solution1 {
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
+    static public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[] { map.get(target - nums[i]), i };
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[] { -1, -1 };
+    }
+}
